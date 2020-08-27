480553aab84ba6262f03f2504ef3774c40076703
Squirtle <tienhoang.pham@bc.fi>
2020-08-27 10:55:51 +0300

    second commit

---
+++ scripts.js
@@ -0,0 +1,39 @@
+function utilities(){
+    /**
+     * addition is a function that is used for adding number of seconds
+     * @param a {Number} number of seconds
+     * @param b {Number} number of raw materials
+     * @memberof utilities
+     * @return {Number} result of the calculation
+     */
+    function addition(a, b){
+        var a = 1; // a is number of seconds
+        return a + 10;
+    }
+
+    var subtraction = function(a, b){
+        var a = 1; // a is number of seconds
+        return a + 10;
+    }
+
+    // ES6 abbreviation for convenience
+    var division = (a, b) => {
+        var a = 1; // a is number of seconds
+        return a + 10;
+    }
+
+    var divisionShorten = a => a + 10 // popular choice for 
+    
+    function convertCelciusToFahrenheit(a = 10) {
+        if (a) {
+            return a + 273;
+        } 
+    }
+
+    convertCelciusToFahrenheit()
+
+    function division_shorten
+
+    divisionShorten()
+    divisionShorten(20) // invoke a function/call a function
+}
